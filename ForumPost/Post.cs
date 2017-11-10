3e1227ea7dec85f0e4d8a7f59a42ea9b0296e5a0
Brent Mifsud <brent.mifsud@outlook.com>
2017-11-09 23:02:28 -0500

    completed forum post exercise

---
+++ ForumPost/Post.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForumPost
+{
+    class Post
+    {
+        //Fields
+        private string title;
+        private string description;
+        private DateTime creationTime;
+        private int voteCount = 1;
+
+        //Properties
+        public string Title
+        {
+            get{return title;}
+            set{title = value;}
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public DateTime CreationTime
+        {
+            get { return creationTime; }
+            set { creationTime = value; }
+        }
+
+        public int VoteCount { get { return voteCount; } }
+
+        //constructor
+        public Post(string title, string description)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.CreationTime = DateTime.Now;
+        }
+
+        public void UpVote()
+        {
+            this.voteCount++;
+        }
+
+        public void DownVote()
+        {
+            this.voteCount--;
+        }
+    }
+}
---
+++ ForumPost/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ForumPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Post post1 = new Post("This is a post", "Test post");
+            post1.UpVote();
+            post1.DownVote();
+            post1.DownVote();
+            post1.DownVote();
+            Console.WriteLine("Post Title: " + post1.Title);
+            Console.WriteLine("Post Desc: " + post1.Description);
+            Console.WriteLine("Post Creation TimeStamp: " + post1.CreationTime.ToString());
+            Console.WriteLine("Post Vote Count: " + post1.VoteCount);
+        }
+    }
+}
